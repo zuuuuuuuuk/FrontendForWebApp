c4755ebfbe3fd1c622a0103a76f8a9e291151dc0
zuuuuuuuuk <zukito5533@gmail.com>
2025-04-29 12:51:07 +0400

    userOrders

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ProductService } from '../services/product.service';
 import { ProductInterface } from '../interfaces/product-interface';
-import { Subscription } from 'rxjs';
+import { Subscription, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-favorites',
@@ -11,39 +11,51 @@ import { Subscription } from 'rxjs';
 })
 export class FavoritesComponent implements OnInit, OnDestroy {
 
+  private subscriptions: Subscription[] = [];
 
-    private subscriptions: Subscription[] = [];
+  userId: number = 0;
+  favProdIds: number[] = [];
+  favProds: ProductInterface[] = [];
 
-userId: number = 0;
-favProdIds: number[] = [];
-favProds: ProductInterface[] = [];
+  constructor(private authService: AuthService, private productService: ProductService) {}
 
-constructor(private authService: AuthService, private productService: ProductService) { }
-  
-ngOnInit(): void {
-   this.getUserId();
-   this.getFavProdIds();
+  ngOnInit(): void {
+    this.userId = this.authService.getUserId();
+    this.getFavProdIds();
   }
 
-getFavProdIds() {
- const favSub = this.productService.getFavorites(this.userId).subscribe({
-next: (response) => {
-  console.log("favs:", response);
-  this.favProdIds = response;
-},
-error: (err) => {
-  console.log("error: ", err)
-}
-  });
-}
+  getFavProdIds() {
+    const favSub = this.productService.getFavorites(this.userId).subscribe({
+      next: (response: number[]) => {
+        console.log("favs:", response);
+        this.favProdIds = response;
+        this.getFavoriteProducts();  // Fetch full product data
+      },
+      error: (err) => {
+        console.log("error: ", err);
+      }
+    });
+    this.subscriptions.push(favSub);
+  }
 
-getUserId(){
- this.userId = this.authService.getUserId();
-}
+  getFavoriteProducts() {
+    if (this.favProdIds.length === 0) return;
 
+    const productRequests = this.favProdIds.map(id =>
+      this.productService.getProductById(id)  // You must have this method in your ProductService
+    );
 
+    const prodSub = forkJoin(productRequests).subscribe({
+      next: (products: ProductInterface[]) => {
+        this.favProds = products;
+      },
+      error: (err) => {
+        console.error("Failed to load favorite products:", err);
+      }
+    });
 
-
+    this.subscriptions.push(prodSub);
+  }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(sub => sub.unsubscribe());
